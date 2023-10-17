c849263a12e52e2f4bd2b08f964e9f3d8cec59af
sombregn <bahalphasouleymane2019@gmail.com>
2023-10-17 20:20:11 +0000

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>Welcome to ProShop</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </>
   )
-}
-
+};
 export default App
